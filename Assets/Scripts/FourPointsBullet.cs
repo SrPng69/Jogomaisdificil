52a0ecf4eb6787847790598a56a3e378a5473ac8
SrPNG69 <gu5t4v0.png@gmail.com>
2026-07-13 14:38:56 -0300

    Indireitamento do script do movimento da esfera de 4 pontos (3 agora)

--- Assets/Scripts/FourPointsBullet.cs
+++ Assets/Scripts/FourPointsBullet.cs
@@ -2,53 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FourPointsBullet : MonoBehaviour
+public class FourPointsBullet /* Três pontos na verdade */ : MonoBehaviour
 {
     public Transform pontoA;
     public Transform pontoB;
     public Transform pontoC;
-    public Transform pontoD;
+    private Transform pontoD; // <--- Sem utilidade por enquanto
     public float Speed = 1;
 
     private float t = 0f;
     private bool indo = true;
 
-    Vector3 inicio = Vector3.zero;
-    Vector3 fim = Vector3.zero;
-
-    private int etapa = 0;
-
     void Update()
     {
-        switch (etapa)
-        {
-            case 0:
-                inicio = pontoA.position;
-                fim = pontoB.position;
-                break;
+        t += Speed * Time.deltaTime;
 
-            case 1:
-                inicio = pontoB.position;
-                fim = pontoC.position;
-                break;
-
-            case 2:
-                inicio = pontoC.position;
-                fim = pontoD.position;
-                break;
-
-            case 3:
-                inicio = pontoD.position;
-                fim = pontoA.position;
-                break;
-        }
-
-        transform.position = Vector3.Lerp(inicio, fim, t);
+        transform.position = Vector3.Lerp(
+            indo ? pontoA.position : pontoB.position,
+            indo ? pontoB.position : pontoC.position,      
+            t
+        );
 
         if (t >= 1f)
         {
             t = 0f;
-            etapa = (etapa + 1) % 4;
+            indo = !indo;
         }
+       
     }
 }
